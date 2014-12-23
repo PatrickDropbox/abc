9e82f150c74a58c9f769bd788bbcd8344da06b4d
Patrick Lee <patrick@dropbox.com>
2014-12-22 21:19:37 -0800

    field skeleton

--- bytecode/attribute_info.scala
+++ bytecode/attribute_info.scala
@@ -1,6 +1,28 @@
 import java.io.DataInputStream
 import java.io.DataOutputStream
+import java.util.Vector
 
+import scala.collection.JavaConversions._
+
+
+object Attribute {
+    def deserialize(
+            inputStream: DataInputStream,
+            constants: ConstantPool): Vector[Attribute] = {
+        val attrCount = inputStream.readUnsignedShort()
+        for (_ <- 1 to attrCount) {
+            // TODO
+            val nameIndex = inputStream.readUnsignedShort()
+            val attrLength = inputStream.readInt()
+            var buffer = new Array[Byte](attrLength)
+            inputStream.readFully(buffer)
+        }
+        return null
+    }
+}
+
+trait Attribute {
+}
 
 class ClassAttributes {
     def serialize(output: DataOutputStream) {
@@ -9,6 +31,7 @@ class ClassAttributes {
 
     def deserialize(input: DataInputStream, constants: ConstantPool) {
         // TODO
+        Attribute.deserialize(input, constants)
     }
 }
 
@@ -19,5 +42,6 @@ class FieldAttributes {
 
     def deserialize(input: DataInputStream, constants: ConstantPool) {
         // TODO
+        Attribute.deserialize(input, constants)
     }
 }
--- bytecode/field_info.scala
+++ bytecode/field_info.scala
@@ -3,8 +3,35 @@ import java.io.DataOutputStream
 
 
 class FieldInfo {
-    var access = new FieldAccessFlags()
-    var name: ConstUtf8Info = null
-    var descriptor: ConstUtf8Info = null
-    var attributes = new FieldAttributes()
+    var _access = new FieldAccessFlags()
+    var _name: ConstUtf8Info = null
+
+    var _descriptorString: ConstUtf8Info = null
+    var _descriptor: FieldType = null
+
+    var _attributes = new FieldAttributes()
+
+    def access(): FieldAccessFlags = _access
+    def name(): String = _name.value()
+    def descriptorString(): String = _descriptorString.value()
+    def descriptor(): FieldType = _descriptor
+    def attributes(): FieldAttributes = _attributes
+
+    def serialize(output: DataOutputStream) {
+        _access.serialize(output)
+        output.writeShort(_name.index)
+        output.writeShort(_descriptorString.index)
+        _attributes.serialize(output)
+    }
+
+    def deserialize(input: DataInputStream, constants: ConstantPool) {
+        _access.deserialize(input)
+        _name = constants.getUtf8ByIndex(input.readUnsignedShort())
+
+        _descriptorString = constants.getUtf8ByIndex(input.readUnsignedShort())
+        var parser = new DescriptorParser(_descriptorString.value())
+        _descriptor = parser.parseFieldDescriptor()
+
+        _attributes.deserialize(input, constants)
+    }
 }
