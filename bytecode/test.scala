0d58ae6947acf9a6f1010beb2602c42920590b5e
Patrick Lee <patrick@dropbox.com>
2014-12-26 02:17:18 -0800

    .

--- bytecode/test.scala
+++ bytecode/test.scala
@@ -13,3 +13,17 @@ class Test {
 
     def h2(): Double = g().toDouble + h()
 }
+
+trait Trait1 {
+    def t1(): Int = 1
+}
+
+trait Trait2 {
+    def t2(): Int = 2
+}
+
+abstract class Abstract1 {
+    def t3(): Int = 3
+}
+
+class Real extends Abstract1 with Trait1 with Trait2 {}
