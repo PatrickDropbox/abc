53dd1370604dabf41b2325731b67e4e75a233c49
Patrick Lee <patrick@dropbox.com>
2014-12-27 19:18:31 -0800

    conversion ops

---
+++ bytecode/conversion_operations.scala
@@ -0,0 +1,48 @@
+import java.io.DataInputStream
+import java.io.DataOutputStream
+
+
+class I2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2L, "i2l") {
+}
+
+class I2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2F, "i2f") {
+}
+
+class I2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2D, "i2d") {
+}
+
+class I2b(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2B, "i2b") {
+}
+
+class I2c(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2C, "i2c") {
+}
+
+class I2s(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2S, "i2s") {
+}
+
+class l2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2I, "l2i") {
+}
+
+class l2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2F, "l2f") {
+}
+
+class l2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2D, "l2d") {
+}
+
+class f2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2I, "f2i") {
+}
+
+class f2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2L, "f2l") {
+}
+
+class f2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2D, "f2d") {
+}
+
+class d2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2I, "d2i") {
+}
+
+class d2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2L, "d2l") {
+}
+
+class d2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2F, "d2f") {
+}
