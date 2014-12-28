c79e1b7d83729229cdc37657b56baefdcb1bc463
Patrick Lee <patrick@dropbox.com>
2014-12-27 19:20:29 -0800

    fixes

--- bytecode/conversion_operations.scala
+++ bytecode/conversion_operations.scala
@@ -20,29 +20,29 @@ class I2c(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2C, "i2c") {
 class I2s(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2S, "i2s") {
 }
 
-class l2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2I, "l2i") {
+class L2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.L2I, "l2i") {
 }
 
-class l2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2F, "l2f") {
+class L2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.L2F, "l2f") {
 }
 
-class l2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.I2D, "l2d") {
+class L2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.L2D, "l2d") {
 }
 
-class f2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2I, "f2i") {
+class F2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2I, "f2i") {
 }
 
-class f2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2L, "f2l") {
+class F2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2L, "f2l") {
 }
 
-class f2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2D, "f2d") {
+class F2d(owner: MethodInfo) extends NoOperandOp(owner, OpCode.F2D, "f2d") {
 }
 
-class d2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2I, "d2i") {
+class D2i(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2I, "d2i") {
 }
 
-class d2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2L, "d2l") {
+class D2l(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2L, "d2l") {
 }
 
-class d2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2F, "d2f") {
+class D2f(owner: MethodInfo) extends NoOperandOp(owner, OpCode.D2F, "d2f") {
 }
