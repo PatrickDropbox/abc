282eabc8e4663995734873941a6207ba0338c327
Patrick Lee <patrick@dropbox.com>
2014-12-31 22:07:12 -0800

    add RewriteClassFile to test serialization

---
+++ bytecode/rewrite_class_file.scala
@@ -0,0 +1,35 @@
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+import scala.collection.JavaConversions._
+
+
+object RewriteClassFile {
+    def main(args: Array[String]) {
+        for (filename <- args) {
+            rewriteClass(filename)
+        }
+    }
+
+    def rewriteClass(filename: String) {
+        println("rewriting class file: " + filename)
+        var input = new DataInputStream(new FileInputStream(filename))
+
+        var classInfo = new ClassInfo()
+        try {
+            classInfo.deserialize(input)
+        } finally {
+            input.close()
+        }
+
+        var output = new DataOutputStream(new FileOutputStream(filename))
+        try {
+            classInfo.serialize(output)
+        } finally {
+            output.close()
+        }
+    }
+}
