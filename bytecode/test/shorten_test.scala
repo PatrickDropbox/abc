c9f8ee56c156f1ab9b5a097728e6dda02e7a7953
Patrick Lee <patrick@dropbox.com>
2015-01-07 01:46:17 -0800

    update test example

--- bytecode/test/shorten_test.scala
+++ bytecode/test/shorten_test.scala
@@ -26,7 +26,30 @@ object ShortenTest {
         var s332 = s33.newSubSection()
 
         var b3321 = s332.newBlock()
-        b3321.returnVoid()
+        b3321.pushI(123)
+        var b3322 = s332.newBlock()
+        b3322.returnVoid()
+
+        var e21 = s2.newExceptionHandle(null)
+        e21.newBlock().returnI()
+
+        var e3a = s3.newExceptionHandle(null)
+        e3a.newBlock().returnL()
+
+        var e3b = s3.newExceptionHandle("Throwable")
+        e3b.newBlock().returnL()
+
+        s3.implicitGoto = null
+
+        var e22 = s22.newExceptionHandle(null)
+        e22.newBlock().returnI()
+
+        b23.implicitGoto = null
+
+        var e332 = s332.newExceptionHandle(null)
+        var e332s = e332.newSubSection()
+        e332s.newBlock().pushI(321)
+        e332s.newBlock().returnVoid()
 
         c.analyze()
 
